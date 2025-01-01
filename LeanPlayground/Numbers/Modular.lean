6b18207bf5626c4e73d5e888227ed4cce5868fae
Gabe Field <gabriel.field@uqconnect.edu.au>
2025-01-01 12:21:03 +1000

    shitpost

--- LeanPlayground/Numbers/Modular.lean
+++ LeanPlayground/Numbers/Modular.lean
@@ -20,54 +20,42 @@ def same_remainder (m : ℤ) : ℤ → ℤ → Prop
 
 -- Establish that `same_remainder` is an `Equivalence` relation
 namespace same_remainder
+  open Numbers.ℤ.results
+
   theorem refl {m : ℤ} (z : ℤ) : same_remainder m z z := by
-    admit
-  -- theorem symm {p q : ℕ × ℕ} : same_difference p q → same_difference q p := by
-  --   intro h ; unfold same_difference at h
-  --   show q.1 + p.2 = p.1 + q.2
-  --   have h : p.1 + q.2 = q.1 + p.2 := h
-  --   exact h.symm
-  -- open ℕ.results in
-  -- theorem trans {p q r : ℕ × ℕ} : same_difference p q → same_difference q r → same_difference p r := by
-  --   intro h_pq h_qr
-  --   have h_pq : p.1 + q.2 = q.1 + p.2 := h_pq
-  --   have h_qr : q.1 + r.2 = r.1 + q.2 := h_qr
-  --   show p.1 + r.2 = r.1 + p.2
-  --   have : p.1 + q.2 + q.1 + r.2 = q.1 + p.2 + r.1 + q.2 := calc p.1 + q.2 + q.1 + r.2
-  --     _ = (p.1 + q.2) + (q.1 + r.2) := by rw [← arithmetic.add_assoc]
-  --     _ = (q.1 + p.2) + (r.1 + q.2) := by rw [h_pq, h_qr]
-  --     _ = q.1 + p.2 + r.1 + q.2     := by rw [arithmetic.add_assoc]
-  --   have : q.1 + (p.1 + q.2 + r.2) = q.1 + (p.2 + r.1 + q.2) :=
-  --     calc q.1 + (p.1 + q.2 + r.2)
-  --       _ = (q.1 + (p.1 + q.2)) + r.2 := by rw [arithmetic.add_assoc]
-  --       _ = ((q.1 + p.1) + q.2) + r.2 := by rw [arithmetic.add_assoc (x := q.1)]
-  --       _ = ((p.1 + q.1) + q.2) + r.2 := by rw [arithmetic.add_comm (x := p.1)]
-  --       _ = (p.1 + (q.1 + q.2)) + r.2 := by rw [← arithmetic.add_assoc (x := p.1)]
-  --       _ = (p.1 + (q.2 + q.1)) + r.2 := by rw [arithmetic.add_comm (x := q.1)]
-  --       _ = p.1 + q.2 + q.1 + r.2     := by rw [arithmetic.add_assoc (x := p.1)]
-  --       _ = q.1 + p.2 + r.1 + q.2     := this
-  --       _ = q.1 + p.2 + (r.1 + q.2)   := by rw [← arithmetic.add_assoc]
-  --       _ = q.1 + (p.2 + (r.1 + q.2)) := by rw [← arithmetic.add_assoc]
-  --       _ = q.1 + (p.2 + r.1 + q.2)   := by rw [arithmetic.add_assoc (x := p.2)]
-  --   have : p.1 + q.2 + r.2 = p.2 + r.1 + q.2 := arithmetic.add_left_cancel this
-  --   have : q.2 + (p.1 + r.2) = q.2 + (p.2 + r.1) :=
-  --     calc q.2 + (p.1 + r.2)
-  --       _ = q.2 + p.1 + r.2   := by rw [arithmetic.add_assoc]
-  --       _ = p.1 + q.2 + r.2   := by rw [@arithmetic.add_comm q.2]
-  --       _ = p.2 + r.1 + q.2   := this
-  --       _ = p.2 + (r.1 + q.2) := by rw [← arithmetic.add_assoc]
-  --       _ = p.2 + (q.2 + r.1) := by rw [@arithmetic.add_comm r.1]
-  --       _ = p.2 + q.2 + r.1   := by rw [arithmetic.add_assoc]
-  --       _ = q.2 + p.2 + r.1   := by rw [@arithmetic.add_comm p.2]
-  --       _ = q.2 + (p.2 + r.1) := by rw [← arithmetic.add_assoc]
-  --   have : p.1 + r.2 = p.2 + r.1 := arithmetic.add_left_cancel this
-  --   conv at this => { rhs; rw [arithmetic.add_comm] }
-  --   assumption
-  -- theorem equivalence : Equivalence same_difference :=
-  --   { refl := refl, symm := symm, trans := trans }
-  -- instance setoid : Setoid (ℕ × ℕ) where
-  --   r := same_difference
-  --   iseqv := equivalence
+    show m ∣ z - z
+    rw [Numbers.ℤ.results.ring.sub_self]
+    exact Numbers.ℤ.results.number_theory.divides_zero m
+  theorem symm {m x y : ℤ} : same_remainder m x y → same_remainder m y x := by
+    intro (h : m ∣ y - x)
+    show m ∣ x - y
+    rw  [ ← number_theory.divides_iff_divides_neg
+        , ring.sub_eq_add_neg
+        , ring.neg_add'
+        , ring.neg_neg
+        , ring.add_comm
+        , ←ring.sub_eq_add_neg]
+    assumption
+  theorem trans {m x y z : ℤ} : same_remainder m x y → same_remainder m y z → same_remainder m x z := by
+    intro (h_xy : m ∣ y - x) (h_yz : m ∣ z - y)
+    show m ∣ z - x
+    conv => {
+      rhs
+      calc z - x
+        _ = z + -x            := by rw [ring.sub_eq_add_neg]
+        _ = z + -x + (-y + y) := by conv => { rhs; rw [ring.neg_add, ring.add_zero] }
+        _ = z + -x + -y + y   := by rw [ring.add_assoc]
+        _ = z + -y + y + -x   := by repeat rw [← ring.add_right_comm (-x)]
+        _ = z - y + (y + -x)  := by conv => { rhs; rw [ring.sub_eq_add_neg, ring.add_assoc]}
+        _ = z - y + (y - x)   := by rw [← ring.sub_eq_add_neg]
+    }
+    apply number_theory.divides_sum <;> assumption
+
+  theorem equivalence {m : ℤ} : Equivalence (same_remainder m) :=
+    { refl := refl, symm := symm, trans := trans }
+  instance setoid {m : ℤ} : Setoid ℤ where
+    r := same_remainder m
+    iseqv := equivalence
 end same_remainder
 
 -- /-- `ℤ / m`, defined as `ℤ ⧸ same_remainder m`. -/
