f505250b5a3706801d9d915db8805780c2ea168f
Gabe Field <gabriel.field@uqconnect.edu.au>
2025-01-12 00:14:04 +1000

    shitpost

--- LeanPlayground/UnivAlg/Basics.lean
+++ LeanPlayground/UnivAlg/Basics.lean
@@ -8,3 +8,17 @@
 
 
 /- SECTION: Equational classes (but not their models) -- inital definitions and setup -/
+
+/-- A logical operation (function symbol), specified by an `.id` and `.arity`. -/
+structure Operation : Type where
+  id    : String
+  arity : Nat
+deriving Repr
+abbrev Op : Type := Operation
+
+/-- A list of operations, each with unique `.id`s. -/
+structure Operations : Type where
+  list : List Operation
+  uqIds : ∀ (i j : Fin list.length), i ≠ j → list.get i ≠ list.get j
+deriving Repr
+abbrev Ops : Type := Operations
