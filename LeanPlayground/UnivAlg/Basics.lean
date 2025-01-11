4a0753e19aa865532b39433f537578e07afe39a8
Gabe Field <gabriel.field@uqconnect.edu.au>
2025-01-12 00:07:52 +1000

    shitpost

---
+++ LeanPlayground/UnivAlg/Basics.lean
@@ -0,0 +1,10 @@
+/- @file LeanPlayground/UnivAlg/Basics.lean
+ - The basic definitions (especially datatypes) behind this theory of universal algebra.
+ -/
+
+/- IMPORTS: -/
+-- (none)
+
+
+
+/- SECTION: Equational classes (but not their models) -- inital definitions and setup -/
